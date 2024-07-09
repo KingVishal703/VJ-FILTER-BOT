8fe6971725746957557983de205ce1a76f98f94f
KingVishal703 <vishal21wankhede@gmail.com>
2024-07-09 23:24:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002248164241'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1029462448 5796857876').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002114485322').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002023570425').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
@@ -73,7 +73,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Hindi_Movies_Search_Group_Bot')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Movies_King_Update')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_AllLinks/19')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+eiygq58NdThhNjg1') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+eiygq58NdThhNjg1') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
