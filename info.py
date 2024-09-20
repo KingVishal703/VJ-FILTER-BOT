dcfc91466c3b5ac6ca36cb14be95e484b2cea74c
KingVishal703 <vishal21wankhede@gmail.com>
2024-09-20 17:27:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-100
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Mk2:gzHqqb0dkK7E0byR@cluster0.x5ppkp8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Mk2:gzHqqb0dkK7E0byR@cluster0.x5ppkp8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0 mongodb+srv://wankedevishal:wXOMJisUbsooyUQI@cluster0.hdd0bbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "MkMovies")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'MkMoviescollection')
 
