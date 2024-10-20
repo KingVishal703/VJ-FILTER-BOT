20046c7d826685bb47091dd64031773cd7d0a019
KingVishal703 <vishal21wankhede@gmail.com>
2024-10-21 00:24:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -95,10 +95,10 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', True))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'shareus.io')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'X5h4uvYH83YSso3xU7vCAjGigZZ2')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '6DRRpJbz96ghKtCBgiORSGMnJ6H3')
 # if verify second shortner is True then fill below url and api
 VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', 'publicearn.com')
 VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '8cb42dcb724c2b970cc10b098132273359eaf0b8')
